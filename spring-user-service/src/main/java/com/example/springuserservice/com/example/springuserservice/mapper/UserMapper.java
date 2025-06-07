de5ecd8a4a7bad58e36dc4ab97529f12e43708f0
shixan <114354917+shixan@users.noreply.github.com>
2025-06-07 18:23:30 +0300

    Add files via upload

---
+++ spring-user-service/src/main/java/com/example/springuserservice/com/example/springuserservice/mapper/UserMapper.java
@@ -0,0 +1,23 @@
+package com.example.springuserservice.mapper;
+
+import com.example.springuserservice.dto.UserDto;
+import com.example.springuserservice.model.User;
+
+public class UserMapper {
+    public static UserDto toDto(User user) {
+        UserDto dto = new UserDto();
+        dto.setId(user.getId());
+        dto.setName(user.getName());
+        dto.setEmail(user.getEmail());
+        dto.setAge(user.getAge());
+        return dto;
+    }
+    public static User toEntity(UserDto dto) {
+        User user = new User();
+        user.setId(dto.getId());
+        user.setName(dto.getName());
+        user.setEmail(dto.getEmail());
+        user.setAge(dto.getAge());
+        return user;
+    }
+}
