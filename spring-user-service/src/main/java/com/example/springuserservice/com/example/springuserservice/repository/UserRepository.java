eeee6be0f3f25c8fc3527876245a92cfa53f1139
shixan <114354917+shixan@users.noreply.github.com>
2025-06-07 18:25:37 +0300

    Delete spring-user-service/src/main/java/com/example/springuserservice/com/example/springuserservice directory

--- spring-user-service/src/main/java/com/example/springuserservice/com/example/springuserservice/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.springuserservice.repository;
-
-import com.example.springuserservice.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
